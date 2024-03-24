55e5b7c664b0bd777d8725bc343b5cb51abaeab2
Erki Ehtla <8049910+erkieh@users.noreply.github.com>
2024-03-24 18:50:50 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,85 @@
+import com.google.protobuf.gradle.*
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.23"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.23"
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.micronaut.application") version "4.3.5"
+    id("com.google.protobuf") version "0.9.2"
+}
+
+version = "0.1"
+group = "com.example"
+
+val kotlinVersion=project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    ksp("io.micronaut.validation:micronaut-validation-processor")
+    ksp("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut:micronaut-discovery-core")
+    implementation("io.micronaut.grpc:micronaut-grpc-runtime")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("javax.annotation:javax.annotation-api")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+}
+
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("21")
+}
+
+
+sourceSets {
+    main {
+        java {
+            srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/java")
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.2"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.61.0"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                // Apply the "grpc" plugin whose spec is defined above, without options.
+                id("grpc")
+            }
+        }
+    }
+}
+micronaut {
+    testRuntime("kotest5")
+    processing {
+        incremental(true)
+        annotations("com.example.*")
+    }
+}
+
+tasks.named<io.micronaut.gradle.docker.MicronautDockerfile>("dockerfile") {
+    baseImage("eclipse-temurin:21-jre-jammy")
+}
+
+tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative") {
+    jdkVersion.set("21")
+}
+
+
